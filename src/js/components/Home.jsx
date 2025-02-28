28cc54e79e3d7536339b2e538c6a5948f46a1674
Rubén Ramírez <rruben084@gmail.com>
2025-02-28 01:56:29 +0000

    Proyecto Semi Terminado

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,21 +1,39 @@
-import React from "react";
-// import React, { useState } from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	// const [color, setColor] = useState("red") 
+	const [color, setColor] = useState("blue");
+
+	const changeColor = () => {
+		const colors = ['red', 'yellow', 'green'];
+		const ramdomColor = colors[Math.floor(Math.random() * colors.length)];
+		console.log(ramdomColor);
+		setColor(ramdomColor);
+	}
 
 	return (
 		<div className="traffic-light">
 			<div className="tube"></div>
 			<div className="box">
-				<div className="red light"></div>
-				<div className="yellow light"></div>
-				<div className="green light"></div>
+				<div
+					// onClick={() => setColor("red")}
+					className={"red light" + (color === "red" ? " glow" : "")}>
+				</div>
+
+				<div
+					// onClick={() => setColor("yellow")}
+					className={"yellow light" + (color === "yellow" ? " glow" : "")}>
+				</div>
+
+				<div
+					// onClick={() => setColor("green")}
+					className={"green light" + (color === "green" ? " glow" : "")}>
+				</div>
 			</div>
+			<button onClick={changeColor} className="button">Clic para cambiar de color</button>
 		</div>
 	);
 };
